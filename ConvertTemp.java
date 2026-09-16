6a6749c55716d6e91c0b7077b63b20a521de217c
AidenXu6 <1548293@learn.vsb.bc.ca>
2026-09-16 14:50:06 -0700

    Add files via upload

---
+++ ConvertTemp.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ConvertTemp{
+	public static void main(String[]args){
+		
+		Scanner in= new Scanner(System.in);
+		System.out.print("Temperature in Celcius:");
+		double celcius=in.nextDouble();
+		double farenheit=(celcius*1.8)+32;
+		System.out.print(farenheit);
+	}
+}
